e07b8c62c62b771a94ab32aa94a865efd41e60bc
Aureo-Bueno <aureoalexandre@outlook.com>
2025-08-14 14:45:25 -0300

    feat: add new mutations and logger

--- Context/Mutation.cs
+++ Context/Mutation.cs
@@ -1,9 +1,17 @@
+using Microsoft.EntityFrameworkCore;
 using SuperHeroesGraphQL.Entities;
 
 namespace SuperHeroesGraphQL.Context;
 
 public class Mutation
 {
+  private readonly ILogger<Mutation> _logger;
+
+  public Mutation(ILogger<Mutation> logger)
+  {
+    _logger = logger;
+  }
+  
   public async Task<SuperHero> AddSuperHero(
     string name,
     string description,
@@ -17,6 +25,7 @@ public class Mutation
     
     context.Set<SuperHero>().Add(hero);
     await context.SaveChangesAsync();
+    _logger.LogInformation($"Added SuperHero {name} to database");
     return hero;
   }
 
@@ -26,14 +35,76 @@ public class Mutation
     string? description,
     [Service] AppDbContext context)
   {
+    _logger.LogInformation($"Finding SuperHero {id}");
     SuperHero? hero = await context.Set<SuperHero>().FindAsync(id);
-    
-    if (hero is null) throw new Exception("SuperHero not found");
 
+    if (hero is null)
+    {
+      _logger.LogError($"Could not find SuperHero {id}");
+      throw new Exception("SuperHero not found");
+    }
+    
     if (name is not null ) hero.Name = name;
     if (description is not null ) hero.Description = description;
+    
+    await context.SaveChangesAsync();
+    _logger.LogInformation($"Updated SuperHero with id: {hero.Id} to database");
+    return hero;
+  }
+  
+  public async Task<Power> AddPower(
+    Guid superHeroId,
+    string? description,
+    string? superPower,
+    [Service] AppDbContext context)
+  {
+    _logger.LogInformation($"Finding Power {superHeroId}");
+    SuperHero? superHero = await context.Set<SuperHero>().FirstOrDefaultAsync(superHero => superHero.Id == superHeroId);
+
+    if (superHero is null)
+    {
+      _logger.LogError($"Could not find SuperHero {superHeroId}");
+      throw new Exception("SuperHero not found");
+    }
+    
+    _logger.LogInformation($"Creating Power on database with super hero id: {superHero.Id}");
+    Power power = new()
+    {
+      Id = Guid.NewGuid(),
+      SuperHero = superHero,
+      Description = description,
+      SuperPower = superPower,
+    };
+    
+    context.Set<Power>().Add(power);
+    await context.SaveChangesAsync();
+    _logger.LogInformation($"Added Power with id: {power.Id} to database");
+    return power;
+  }
+  
+  public async Task<Power> UpdatePower(
+    Guid id,
+    Guid? superHeroId,
+    string? description,
+    string? superPower,
+    [Service] AppDbContext context)
+  {
+    _logger.LogInformation($"Finding Power {id}");
+    Power? power = await context.Set<Power>().FindAsync(id);
+
+    if (power is null)
+    {
+      _logger.LogError($"Could not find Power {id}");
+      throw new Exception("Power not found");
+    }
+
+    _logger.LogInformation($"Updating Power with id: {power.Id} to database");
+    if (superHeroId is not null ) power.SuperHero = await context.Set<SuperHero>().FirstOrDefaultAsync(superHero => superHero.Id == superHeroId);
+    if (description is not null ) power.Description = description;
+    if (superPower is not null) power.SuperPower = superPower;
 
     await context.SaveChangesAsync();
-    return hero;
+    _logger.LogInformation($"Updated Power with id: {power.Id} to database");
+    return power;
   }
 }
--- Context/Query.cs
+++ Context/Query.cs
@@ -3,6 +3,14 @@ using SuperHeroesGraphQL.Entities;
 namespace SuperHeroesGraphQL.Context;
 public class Query
 {
+  
+  private readonly ILogger<Query> _logger;
+
+  public Query(ILogger<Query> logger)
+  {
+    _logger = logger;
+  }
+  
   [UseProjection]
   [UseFiltering]
   [UseSorting]
